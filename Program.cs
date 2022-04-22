3a0270be65c7b47a5670c9a82c21b54a5ded4a4d
Alok Kushwaha <95715756+thefriendlyprogrammer@users.noreply.github.com>
2022-04-22 14:42:53 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Prime_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prime Number");
+            int n, m=0, i, flag=0;
+            Console.WriteLine("Enter the Number to Ckeck Prime: ");
+            n=int.Parse(Console.ReadLine());
+            m=n/2;
+            for(i=2; i<=m; i++)
+            {
+                if(n % i == 0)
+                {
+                    Console.WriteLine("Number is not Prime");
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag == 0)
+            {
+                Console.WriteLine("Number is Prime");
+            }
+            Console.ReadLine();
+        }
+    }
+}
